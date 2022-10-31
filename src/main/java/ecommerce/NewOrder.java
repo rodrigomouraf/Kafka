6501b0d546eef9857f39a0885752eee6ae364f87
rodrigomouraf <rodrigomoura_rm@hotmail.com>
2022-10-31 01:32:03 -0300

    feat: consumer kafka

--- src/main/java/ecommerce/NewOrder.java
+++ src/main/java/ecommerce/NewOrder.java
@@ -1,46 +1,21 @@
 package ecommerce;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrder {
     public static void main (String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var value = "3,5,43";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null){
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println(
-                    "sucesso enviado " + data.topic() +
-                            ":::partition "  + data.partition() +
-                            "/ offset " + data.offset() +
-                            "/ timestamp" + data.timestamp()
-                );
-        }).get();
-    }
+        try(var dispatcher = new KafkaDispatcher())
+        {
+            for(var i = 0; i <  10; i++){
+                var key = UUID.randomUUID().toString();
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "127.0.0.1:9092"
-        );
-        properties.setProperty(
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class.getName()
-        );
-        properties.setProperty(
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class.getName()
-        );
-        return properties;
+                var value = key + "31,37";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+
+                var email = "Thank you for your order! We are processing your order!";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, email);
+            }
+        }
     }
 }
