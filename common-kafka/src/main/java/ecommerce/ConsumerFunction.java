dce9561bd701608fd0d3ae11c889178b522bceae
rodrigomouraf <rodrigomoura_rm@hotmail.com>
2022-11-03 19:55:08 -0300

    feat: add create new users

--- common-kafka/src/main/java/ecommerce/ConsumerFunction.java
+++ common-kafka/src/main/java/ecommerce/ConsumerFunction.java
@@ -3,5 +3,6 @@ package ecommerce;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public interface ConsumerFunction<T> {
-    void consume(ConsumerRecord<String, T> record);
+    void consume(ConsumerRecord<String, T> record) throws Exception;
 }
+
--- service-fraud-detector/src/main/java/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/ecommerce/FraudDetectorService.java
@@ -2,10 +2,11 @@ package ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService {
-
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
         try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
@@ -17,7 +18,9 @@ public class FraudDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
@@ -30,7 +33,20 @@ public class FraudDetectorService {
             // ignoring
             e.printStackTrace();
         }
+        var order = record.value();
+        if(isFraud(order)){
+            //pretending that the fraud happens when the amount is >= 4500
+            System.out.println("Order is a Fraud!!! " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+        }else{
+            System.out.println("Approved: " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+        }
         System.out.println("Order processed");
+    }
+
+    private static boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 
 }
